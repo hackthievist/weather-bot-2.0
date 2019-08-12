b8481c51c2dc9280c9a1d459eda90797c7e13845
hackthievist <sobogunifeoluwa@gmail.com>
2019-08-12 20:39:47 +0100

    :recycle: refactor(refactor page title): rename page title for views

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ze-weather v2' });
 });
 
 router.post('/tweet', TweetController.tweet);
